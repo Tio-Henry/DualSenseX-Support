b3755336de231f4e8dc38fc5a7b714ea17988044
Tio-Henry <H3nry.g147@gmail.com>
2025-09-21 11:47:09 -0300

    small update

--- main.gd
+++ main.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("DSXRef","res://addons/dualsensex/api/reference.gd")
-	add_autoload_singleton("DSXSys","res://addons/dualsensex/api/system.gd")
+	add_autoload_singleton("DSXRef","res://addons/dualsensex_support/api/reference.gd")
+	add_autoload_singleton("DSXSys","res://addons/dualsensex_support/api/system.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("DSXRef")
