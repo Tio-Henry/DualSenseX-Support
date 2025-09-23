290de33564e683200e8187c076c6922334a4b3c9
Tio-Henry <H3nry.g147@gmail.com>
2025-09-22 23:20:59 -0300

    Init work with Triggers and added method connect_to for connect with DSX using ip and port

--- api/reference.gd
+++ api/reference.gd
@@ -21,3 +21,53 @@ enum MicLED {
 	PULSE,
 	OFF
 }
+
+enum TriggerMode {
+		Normal,
+		GameCube,
+		VerySoft,
+		Soft,
+		Hard,
+		VeryHard,
+		Hardest,
+		Rigid,
+		VibrateTrigger,
+		Choppy,
+		Medium,
+		VibrateTriggerPulse,
+		CustomTriggerValue,
+		Resistance,
+		Bow,
+		Galloping,
+		SemiAutomaticGun,
+		AutomaticGun,
+		Machine,
+		VIBRATE_TRIGGER_10Hz,
+		OFF,
+		FEEDBACK,
+		WEAPON,
+		VIBRATION,
+		SLOPE_FEEDBACK,
+		MULTIPLE_POSITION_FEEDBACK,
+		MULTIPLE_POSITION_VIBRATION
+		}
+
+enum CustomTriggerValueMode {
+		OFF,
+		Rigid,
+		RigidA,
+		RigidB,
+		RigidAB,
+		Pulse,
+		PulseA,
+		PulseB,
+		PulseAB,
+		VibrateResistance,
+		VibrateResistanceA,
+		VibrateResistanceB,
+		VibrateResistanceAB,
+		VibratePulse,
+		VibratePulseA,
+		VibratePulsB,
+		VibratePulseAB
+		}
--- api/system.gd
+++ api/system.gd
@@ -1,19 +1,22 @@
 extends Node
 
-var DSXServer : PacketPeerUDP = PacketPeerUDP.new()
+var DSXServer: PacketPeerUDP = PacketPeerUDP.new()
 
 func _enter_tree() -> void:
 	DSXRef.process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
 	process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
-	DSXServer.connect_to_host("127.0.0.1",6971)
+	connect_to("127.0.0.1",6971)
 
 func _exit_tree() -> void:
 	reset_settings(0)
 
 func _ready() -> void:
-	led(0, Color(0.733, 0.0, 0.537, 1.0),10)
+	led(0, Color(0.733, 0.0, 0.537, 1.0),25)
 	mic_led(0, DSXRef.MicLED.ON)
 	print(get_dsx_info())
+
+func connect_to(ip: String, port: int) -> Error:
+	return DSXServer.connect_to_host(ip, port)
 
 func get_dsx_info() -> String:
 	send_data({"type":DSXRef.InstructionType.GetDSXStatus})
@@ -25,8 +28,8 @@ func get_dsx_info() -> String:
 	else:
 		return ""
 
-#func adaptive_trigger(device: int, trigger: DSXRef.Trigger) -> void:
-	#send_data({"type":DSXRef.InstructionType.TriggerUpdate,"parameters":[device,trigger,22,2,6,8]})
+func adaptive_trigger(device: int, trigger: DSXRef.Trigger, trigger_mode: DSXRef.TriggerMode) -> void:
+	send_data({"type":DSXRef.InstructionType.TriggerUpdate,"parameters":[device,trigger,trigger_mode,2,6,8]})
 	
 func led(device: int, color: Color, brightness: int) -> void:
 	send_data({"type":DSXRef.InstructionType.RGBUpdate,"parameters":[device, color.r8, color.g8, color.b8, brightness]})
